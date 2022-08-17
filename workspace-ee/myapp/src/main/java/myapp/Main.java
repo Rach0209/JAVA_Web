0e0db98ad9b3748a6eab383f130acf01577faf58
you_tu <104557592+Rach0209@users.noreply.github.com>
2022-08-17 17:55:28 +0900

    Add files via upload

---
+++ workspace-ee/myapp/src/main/java/myapp/Main.java
@@ -0,0 +1,13 @@
+package myapp;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+	private static final Logger logger = LoggerFactory.getLogger(Main.class);
+	
+	public static void main(String[] args) {
+//		System.out.println("Hello World!");
+		logger.info("Hello World~~");
+	}
+}
