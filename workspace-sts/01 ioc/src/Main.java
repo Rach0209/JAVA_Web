0a69795ddd152043aa03863fcd50aa31a4a612b1
you_tu <104557592+Rach0209@users.noreply.github.com>
2022-09-02 09:21:53 +0900

    Add files via upload

--- workspace-sts/01 ioc/src/Main.java
+++ workspace-sts/01 ioc/src/Main.java
@@ -8,6 +8,7 @@ public class Main {
 		obj.hello();
 		
 //		설정하기 전에 실행하면, NullPointerException 발생
+//		Spring Framework에서는 의존성의 역전을 통해 필요할때마다 설정만 해주면 사용할 수 있다.
 		MyLogic logic = new MyLogic(new MyBean());
 		logic.someMethod();
 	}
